e9027ff027783ed2fb420f5a1e73912db60a1a03
david.tian <david.tian@live.com>
2020-03-22 14:58:20 +0800

    LinkedHashSet的用法

---
+++ day17_Set/src/cn/dvtn_04/LinkedHashSetTest.java
@@ -0,0 +1,27 @@
+package cn.dvtn_04;
+
+import java.util.LinkedHashSet;
+
+/**
+ * LinkedHashSet: 底层数据结构有哈希表和链表组成
+ * 哈希表保证元素唯一性
+ * 链表保证元素有序
+ *
+ */
+public class LinkedHashSetTest {
+    public static void main(String[] args) {
+        LinkedHashSet<String> lhs = new LinkedHashSet<String>();
+        lhs.add("Hello");
+        lhs.add("World");
+        lhs.add("Java");
+        lhs.add("Android");
+        lhs.add("Hello");
+        lhs.add("World");
+        lhs.add("Java");
+        lhs.add("Android");
+
+        for(String s: lhs){
+            System.out.println(s);
+        }
+    }
+}
