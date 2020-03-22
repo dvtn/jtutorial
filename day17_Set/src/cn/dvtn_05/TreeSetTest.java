707e5a355a887089d63bd51e908146874fc7f3a1
david.tian <david.tian@live.com>
2020-03-22 15:32:09 +0800

    TreeSet的用法

--- day17_Set/src/cn/dvtn_05/TreeSetTest.java
+++ day17_Set/src/cn/dvtn_05/TreeSetTest.java
@@ -4,8 +4,8 @@ import java.util.TreeSet;
 
 /**
  * TreeSet: 基于TreeMap的NavigableSet实现
- *      使用元素的自然顺序对元素进行排序(自然排序）
- *      或者创建set时提供的Comparator进行排序，具体取决于构造方法（比较器排序)
+ *      使用元素的自然顺序对元素进行排序(自然排序Comparable）: compareTo方法，这个方法是定义在接口Comparable接口
+ *      或者创建set时提供的Comparator进行排序，具体取决于构造方法（比较器排序Comparator)
  *
  *   排序并保证唯一性
  *
@@ -16,6 +16,8 @@ import java.util.TreeSet;
  * TreeSet(Collection<? extends E> c> 构造一个包含指定元素的新TreeSet,它按照元素的自然顺序排序
  * TreeSet(Comparator<? supper E> comparator): 构造一个新的空TreeSet, 它根据指定比较器进行排序。
  *
+ *
+ * TreeSet的add()方法，最终要看TreeMap的put方法。
  *
  */
 
