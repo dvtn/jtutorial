ee585effa431daabdc59450e99d2db98e7a61eca
david.tian <david.tian@live.com>
2020-03-22 13:46:45 +0800

    HashSet的用法

--- day17_Set/src/cn/dvtn_02/HashSetTest.java
+++ day17_Set/src/cn/dvtn_02/HashSetTest.java
@@ -4,6 +4,12 @@ import java.util.HashSet;
 
 /**
  * HashSet存储字符串并遍历
+ *
+ * 能过查看add的源码这个方法底层依赖两个方法：hashCode()和equals()
+ * 步骤：
+ *      首先比较哈希值
+ *      如果相同，继续走，比较地址值或者equals方法
+ *      如果不同，添加到集合中
  */
 
 public class HashSetTest {
