7e47564aaa17a86dcda111d7fc058b7406c5071e
david.tian <david.tian@live.com>
2020-03-22 09:46:41 +0800

    用户基本描述类

---
+++ day17_LoginAndRegister/src/cn/dvtn/pojo/User.java
@@ -0,0 +1,37 @@
+package cn.dvtn.pojo;
+
+/**
+ * 用户基本描述类
+ * @author 独孤求败
+ * @version v1.0
+ * @mail david.tian@live.com
+ */
+
+public class User {
+    private String username;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
