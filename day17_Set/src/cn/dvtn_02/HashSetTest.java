7bf1253717af1c9a0331229b459fb01b3a4d342d
david.tian <david.tian@live.com>
2020-03-22 10:46:53 +0800

    HashSet的用法

--- day17_Set/src/cn/dvtn_02/HashSetTest.java
+++ day17_Set/src/cn/dvtn_02/HashSetTest.java
@@ -1,4 +1,28 @@
 package cn.dvtn_02;
 
+import java.util.HashSet;
+
+/**
+ * HashSet存储字符串并遍历
+ */
+
 public class HashSetTest {
+    public static void main(String[] args) {
+        //创建集合对象
+        HashSet<String> hs = new HashSet<String>();
+        //新建元素并添加
+        hs.add("独孤求败");
+        hs.add(("风清扬"));
+        hs.add("东方不败");
+        hs.add("任我行");
+        hs.add("独孤求败");
+        hs.add(("风清扬"));
+        hs.add("东方不败");
+        hs.add("任我行");
+        //遍历
+        for(String s: hs){
+            System.out.println(s);
+        }
+
+    }
 }
