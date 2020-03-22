0d2e7836dc771d6b0a85e58d6befcc7edddf91bd
david.tian <david.tian@live.com>
2020-03-22 22:39:54 +0800

    自定义比较器实现类

---
+++ day17_Set/src/cn/dvtn_07/MyComparator.java
@@ -0,0 +1,10 @@
+package cn.dvtn_07;
+
+import java.util.Comparator;
+
+public class MyComparator implements Comparator {
+    @Override
+    public int compare(Object o1, Object o2) {
+        return 0;
+    }
+}
