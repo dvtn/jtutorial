6b9905719a2f9b2a32609dc42abb275a511329b8
david.tian <david.tian@live.com>
2020-03-22 22:38:38 +0800

    创建学生类，用于TreeSet比较器排序

---
+++ day17_Set/src/cn/dvtn_07/Student.java
@@ -0,0 +1,34 @@
+package cn.dvtn_07;
+
+/**
+ * 如果一类的元素要想能够自然排序，必须实现自然排序接口Comparable
+ */
+public class Student {
+    private String name;
+    private int age;
+
+    public Student() {
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+}
